cd43d036224e87b04bc10b7208a93a9fb803d531
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-10-19 23:40:34 +0800

    Update word_cluster.py

--- word_cluster.py
+++ word_cluster.py
@@ -1,42 +1,36 @@
 #! -*- coding: utf-8 -*-
 # 基于Word2Vec词向量的词聚类例子
 
+import uniout
+import numpy as np
 from gensim.models import Word2Vec
 from tqdm import tqdm
 from infomap import infomap
 
 
-word2vec = Word2Vec.load('word2vec_baike')
-word2vec.init_sims()
+num_words = 10000 # 只保留前10000个词
 min_sim = 0.6
+
+
+word2vec = Word2Vec.load('word2vec_baike')
+
+word_vecs = word2vec.wv.syn0[:num_words]
+word_vecs /= (word_vecs**2).sum(axis=1, keepdims=True)**0.5
+id2word = word2vec.wv.index2word[:num_words]
+word2id = {j: i for i, j in enumerate(id2word)}
+
 links = {}
-wordset = set(word2vec.wv.index2word[:10000]) # 只保留前10000个词
-
-# 每个词找与它相似度不小于0.5的词（不超过40个），来作为图上的边
-for u in tqdm(wordset):
-    for v, sim in word2vec.most_similar(u, topn=50):
-        if v in wordset:
-            if sim >= min_sim:
-                links[(u, v)] = sim
-            else:
-                break
 
 
-_word2id_mapping = {}
-_id2word_mapping = {}
-
-
-def word2id(w):
-    if w in _word2id_mapping:
-        return _word2id_mapping[w]
-    else:
-        _id2word_mapping[len(_word2id_mapping)] = w
-        _word2id_mapping[w] = len(_word2id_mapping)
-        return _word2id_mapping[w]
-
-
-def id2word(i):
-    return _id2word_mapping.get(i, '')
+# 每个词找与它相似度不小于0.6的词（不超过50个），来作为图上的边
+for i in tqdm(range(num_words)):
+    sims = np.dot(word_vecs, word_vecs[i])
+    idxs = sims.argsort()[::-1][1:]
+    for j in idxs[:50]:
+        if sims[j] >= min_sim:
+            links[(i, j)] = float(sims[j])
+        else:
+            break
 
 
 infomapWrapper = infomap.Infomap("--two-level --directed")
@@ -45,7 +39,7 @@ infomapWrapper = infomap.Infomap("--two-level --directed")
 
 
 for (i, j), sim in tqdm(links.items()):
-    _ = infomapWrapper.addLink(word2id(i), word2id(j), sim)
+    _ = infomapWrapper.addLink(i, j, sim)
 
 infomapWrapper.run()
 tree = infomapWrapper.tree
@@ -54,19 +48,18 @@ tree = infomapWrapper.tree
 word2class = {}
 class2word = {}
 for node in tree.leafIter():
-    if id2word(node.physIndex) not in word2class:
-        word2class[id2word(node.physIndex)] = []
-    word2class[id2word(node.physIndex)].append(node.moduleIndex())
+    if id2word[node.physIndex] not in word2class:
+        word2class[id2word[node.physIndex]] = []
+    word2class[id2word[node.physIndex]].append(node.moduleIndex())
     if node.moduleIndex() not in class2word:
         class2word[node.moduleIndex()] = []
-    class2word[node.moduleIndex()].append(id2word(node.physIndex))
+    class2word[node.moduleIndex()].append(id2word[node.physIndex])
 
 
 # len([(k, v) for k, v in word2class.items() if len(v) > 1])
 
 
-import uniout
-
 for i in range(100):
     print class2word[i]
     print
+
